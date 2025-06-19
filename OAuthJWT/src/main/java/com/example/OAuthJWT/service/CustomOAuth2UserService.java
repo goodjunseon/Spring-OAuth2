995141ea0747fb0e76e5a21bd04affc33a06bcc7
goodjunseon <pzs2001926@gmail.com>
2025-06-19 20:02:56 +0900

    [11]유저 정보 DB저장

--- OAuthJWT/src/main/java/com/example/OAuthJWT/service/CustomOAuth2UserService.java
+++ OAuthJWT/src/main/java/com/example/OAuthJWT/service/CustomOAuth2UserService.java
@@ -1,6 +1,8 @@
 package com.example.OAuthJWT.service;
 
 import com.example.OAuthJWT.dto.*;
+import com.example.OAuthJWT.entity.UserEntity;
+import com.example.OAuthJWT.repository.UserRepository;
 import org.springframework.security.oauth2.client.userinfo.DefaultOAuth2UserService;
 import org.springframework.security.oauth2.client.userinfo.OAuth2UserRequest;
 import org.springframework.security.oauth2.core.OAuth2AuthenticationException;
@@ -9,6 +11,12 @@ import org.springframework.stereotype.Service;
 
 @Service
 public class CustomOAuth2UserService extends DefaultOAuth2UserService {
+
+    private final UserRepository userRepository;
+
+    public CustomOAuth2UserService(UserRepository userRepository){
+        this.userRepository = userRepository;
+    }
 
     @Override
     public OAuth2User loadUser(OAuth2UserRequest userRequest) throws OAuth2AuthenticationException {
@@ -28,14 +36,39 @@ public class CustomOAuth2UserService extends DefaultOAuth2UserService {
            return null;
         }
 
-        // 추후 작성
         String username = oAuth2Response.getProvider()+ " " + oAuth2Response.getProviderId();
+        UserEntity existData = userRepository.findByUsername(username);
 
-        UserDTO userDTO = new UserDTO();
-        userDTO.setUsername(username);
-        userDTO.setName(oAuth2Response.getName());
-        userDTO.setRole("ROLE_USER");
+        if (existData == null) {
 
-        return new CustomOAuth2User(userDTO);
+            UserEntity userEntity = new UserEntity();
+            userEntity.setUsername(username);
+            userEntity.setEmail(oAuth2Response.getEmail());
+            userEntity.setName(oAuth2Response.getName());
+            userEntity.setRole("ROLE_USER");
+
+            userRepository.save(userEntity);
+
+            UserDTO userDTO = new UserDTO();
+            userDTO.setUsername(username);
+            userDTO.setName(oAuth2Response.getName());
+            userDTO.setRole("ROLE_USER");
+
+            return new CustomOAuth2User(userDTO);
+
+        } else {
+
+            existData.setEmail(oAuth2Response.getEmail());
+            existData.setName(oAuth2Response.getName());
+
+            userRepository.save(existData);
+
+            UserDTO userDTO = new UserDTO();
+            userDTO.setUsername(existData.getUsername());
+            userDTO.setName(oAuth2Response.getName());
+            userDTO.setRole(existData.getRole());
+
+            return new CustomOAuth2User(userDTO);
+        }
     }
 }
