98699ae53fab513c7220fb37228ce28618a7b5cf
goodjunseon <pzs2001926@gmail.com>
2025-06-19 22:14:11 +0900

    [14]JWT 검증 필터

--- OAuthJWT/src/main/java/com/example/OAuthJWT/config/SecurityConfig.java
+++ OAuthJWT/src/main/java/com/example/OAuthJWT/config/SecurityConfig.java
@@ -6,7 +6,6 @@ import com.example.OAuthJWT.oauth2.CustomSuccessHandler;
 import com.example.OAuthJWT.service.CustomOAuth2UserService;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
